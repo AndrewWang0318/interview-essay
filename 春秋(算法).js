e2027aeae02258c03fca60e187aacec291a926ff
WangNing <AndrewWang0318@outlook.com>
2024-01-23 13:59:26 +0800

    .

--- 春秋(算法).js
+++ 春秋(算法).js
@@ -42,22 +42,30 @@ const node1 = new ListNode(1);
 const node2 = new ListNode(2);
 const node3 = new ListNode(3)
 
-// 添加链表元素
+/* 添加链表元素 */
 head.next = node1
 node1.next = node2
 
-// 插入链表元素：将node3插入到node1与node2之间, 重心在目标节点的前驱节点
+/* 插入链表元素：将node3插入到node1与node2之间, 重心在目标节点的前驱节点 */
 node3.next = node1.next;
 node1.next = node3;
 
-// 删除链表元素node3；只需要遍历时可以跳过目标元素即可 (将目标元素的前驱元素的next等于 后驱元素的next)
+/*  删除链表元素node3；只需要遍历时可以跳过目标元素即可 (将目标元素的前驱元素的next等于 后驱元素的next) */
 node1.next = node3.next
 
-// 遍历链表,访问链表
-let target = head
-const idx = 2
+/*  遍历链表,访问链表 */
+let target = head;
+const idx = 2;
 for (let i = 0; i < idx; i++) {
   target = target.next
 }
+console.log(target)
 
-console.log(target.val)
+// 二叉树
+/* 树由根节点 节点 叶节点 组成；从叶节点到根节点的层级为高度,一个节点开叉多少个子树被称为度 */
+function TreeNode(val = null){
+  this.val = val;
+  this.left = this.right = null;
+}
+const tree = new TreeNode(1);
+console.log(tree)
